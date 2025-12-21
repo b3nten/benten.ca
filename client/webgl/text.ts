37f27b8aab4efd3b997ddafd2d4b2bcb6690b7b1
benton <ben@benten.ca>
2025-12-20 20:45:36 -0800

    add new text

--- client/webgl/text.ts
+++ client/webgl/text.ts
@@ -1,58 +1,119 @@
-import { ActiveCameraComponent, ActorComponent, createPrefab, Transform } from "elysiatech";
-import { Text } from 'troika-three-text'
+import {ActiveCameraComponent, ActorComponent, createPrefab, Transform} from "elysiatech";
+import {Text} from 'troika-three-text'
 import * as Three from "three"
 
 export const textPrefab = createPrefab(world => {
 
-	const myText = new Text()
+    const rootEntity = world.createEntity()
 
-	// Set properties to configure:
-	myText.text = 'benton'
-	myText.fontSize = 6
-	myText.position.z = -2
-	myText.material = new Three.MeshStandardMaterial({
-		color: "white",
-		emissive: "white",
-		emissiveIntensity: 5,
-	})
-	myText.sync()
+    const bentonEntity = world.createEntity()
+    {
+        const bentonText = new Text()
+        bentonText.text = 'benton'
+        bentonText.fontSize = 6
+        bentonText.position.z = -2
+        bentonText.material = new Three.MeshStandardMaterial({
+            color: "white",
+            emissive: "white",
+            emissiveIntensity: 10,
+        })
+        bentonText.sync()
 
-	const vec3 = new Three.Vector3;
-	// 24 pixels from top left corner
-	const POS_X = 24;
-	const POS_Y = 24;
-	const TARGET_Z = -20;
+        const vec3 = new Three.Vector3;
+        const POS_X = 24;
+        const POS_Y = 24;
+        const TARGET_Z = -20;
 
-	return world.createEntityWith(
-		ActorComponent.create({
-			update(_, entity, world) {
-				const transform = world.getComponent(entity, Transform)!;
-				for (const [,,camera] of world.componentIterator(Transform, Three.PerspectiveCamera, ActiveCameraComponent)) {
-					const ndcx = (POS_X / window.innerWidth) * 2 - 1;
-					const ndcy = -(POS_Y / window.innerHeight) * 2 + 1;
+        world.addComponents(bentonEntity,
+            ActorComponent.create({
+                update(_, entity, world) {
+                    const transform = world.getComponent(entity, Transform)!;
+                    for (const [, , camera] of world.componentIterator(Transform, Three.PerspectiveCamera, ActiveCameraComponent)) {
+                        const ndcx = (POS_X / window.innerWidth) * 2 - 1;
+                        const ndcy = -(POS_Y / window.innerHeight) * 2 + 1;
 
-					vec3.set(ndcx, ndcy, 0.5);
-					vec3.unproject(camera);
+                        vec3.set(ndcx, ndcy, 0.5);
+                        vec3.unproject(camera);
 
-					vec3.sub(camera.position).normalize();
+                        vec3.sub(camera.position).normalize();
 
-					const t = (TARGET_Z - camera.position.z) / vec3.z;
+                        const t = (TARGET_Z - camera.position.z) / vec3.z;
 
-					const finalX = camera.position.x + t * vec3.x;
-					const finalY = camera.position.y + t * vec3.y;
+                        const finalX = camera.position.x + t * vec3.x;
+                        const finalY = camera.position.y + t * vec3.y;
 
-					transform.setPosition(finalX, finalY, TARGET_Z);
-					transform.setScale(
-						window.innerWidth / 1000,
-						window.innerWidth / 1000,
-						1
-					)
+                        transform.setPosition(finalX, finalY, TARGET_Z);
+                        transform.setScale(
+                            window.innerWidth / 1000,
+                            window.innerWidth / 1000,
+                            1
+                        )
 
-					break
-				}
-			}
-		}),
-		new Transform().setPosition(0, 0, -20),
-		myText
-	)
+                        break
+                    }
+                }
+            }),
+            new Transform().setPosition(0, 0, -20),
+            bentonText
+        )
+
+        world.parent(rootEntity, bentonEntity);
+    }
+
+    const blurbEntity = world.createEntity()
+    {
+        const text = new Text()
+        text.text = 'The digital home of\nBenton Boychuk-Chorney'
+        text.fontSize = .75
+        text.position.z = -2
+        text.alignment = 'centred'
+        text.material = new Three.MeshStandardMaterial({
+            color: "white",
+            emissive: "white",
+            emissiveIntensity: 10,
+        })
+        text.sync()
+
+        const vec3 = new Three.Vector3;
+        const POS_X = window.innerWidth - 800;
+        const POS_Y = window.innerHeight - 250;
+        const TARGET_Z = -20;
+
+        world.addComponents(blurbEntity,
+            ActorComponent.create({
+                update(_, entity, world) {
+                    const transform = world.getComponent(entity, Transform)!;
+                    for (const [, , camera] of world.componentIterator(Transform, Three.PerspectiveCamera, ActiveCameraComponent)) {
+                        const ndcx = (POS_X / window.innerWidth) * 2 - 1;
+                        const ndcy = -(POS_Y / window.innerHeight) * 2 + 1;
+
+                        vec3.set(ndcx, ndcy, 0.5);
+                        vec3.unproject(camera);
+
+                        vec3.sub(camera.position).normalize();
+
+                        const t = (TARGET_Z - camera.position.z) / vec3.z;
+
+                        const finalX = camera.position.x + t * vec3.x;
+                        const finalY = camera.position.y + t * vec3.y;
+
+                        transform.setPosition(finalX, finalY, TARGET_Z);
+                        transform.setScale(
+                            window.innerWidth / 1000,
+                            window.innerWidth / 1000,
+                            1
+                        )
+
+                        break
+                    }
+                }
+            }),
+            new Transform().setPosition(0, 0, -20),
+            text
+        )
+
+        world.parent(rootEntity, blurbEntity);
+    }
+
+    return rootEntity;
 })
